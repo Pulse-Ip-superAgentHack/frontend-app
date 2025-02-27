bd5c9d234fcf741503daa90f602ffb2a4f0e82d7
Phong Si <phong.si@gmail.com>
2025-02-26 17:09:27 -0700

    add sell btn to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,12 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { getTokens } from '@/utils/tokenStorage'
 
+import { IpMetadata, LicenseTerms } from '@story-protocol/core-sdk'
+import { client } from '../../utils/storyUtils'
+import { uploadJSONToIPFS } from '../../utils/uploadToIpfs'
+import { createHash } from 'crypto'
+import { Address, zeroAddress, zeroHash } from 'viem'
+
 export default function DashboardPage() {
   const [dashboardData, setDashboardData] = useState<any>(null)
   const [loading, setLoading] = useState(true)
@@ -84,6 +90,106 @@ export default function DashboardPage() {
   
   const handleViewRawData = () => {
     router.push('/raw-data')
+  }
+
+  const [msg1, setMsg1] = useState(""); // Track msg1
+  const [msg2, setMsg2] = useState(""); // Track msg2
+  const [txlink, setTxlink] = useState(""); // Track txlink
+
+  const mintIp = async () => {
+    if (!txlink) {
+    const commercialRemixTerms: LicenseTerms = {
+      transferable: true,
+      royaltyPolicy: '0xBe54FB168b3c982b7AaE60dB6CF75Bd8447b390E', // RoyaltyPolicyLAP address from https://docs.story.foundation/docs/deployed-smart-contracts
+      defaultMintingFee: BigInt(100),
+      expiration: BigInt(0),
+      commercialUse: true,
+      commercialAttribution: true,
+      commercializerChecker: zeroAddress,
+      commercializerCheckerData: zeroAddress,
+      commercialRevShare: 100, // can claim 100% of derivative revenue
+      commercialRevCeiling: BigInt(0),
+      derivativesAllowed: true,
+      derivativesAttribution: true,
+      derivativesApproval: false,
+      derivativesReciprocal: true,
+      derivativeRevCeiling: BigInt(0),
+      currency: '0x1514000000000000000000000000000000000000', // $WIP address from https://docs.story.foundation/docs/deployed-smart-contracts
+      uri: '',
+    }
+  
+    const licensingConfig = {
+      isSet: false,
+      mintingFee: BigInt(100),
+      licensingHook: zeroAddress,
+      hookData: zeroHash,
+      commercialRevShare: 100,
+      disabled: false,
+      expectMinimumGroupRewardShare: 0,
+      expectGroupRewardPool: zeroAddress,
+    };
+  
+    const ipMetadata = {
+      "title": "Fitness AI Agent",
+      "description": "This is an example test eliza AI Agent registered on Story.",
+      "createdAt": "1740005219",
+      "creators": [
+        {
+          "name": "JJ T",
+          "address": `${process.env.NEXT_PUBLIC_WALLET_ADDRESS}`,
+          "contributionPercent": 100
+        }
+      ],
+      "image": "https://ipfs.io/ipfs/bafybeigi3k77t5h5aefwpzvx3uiomuavdvqwn5rb5uhd7i7xcq466wvute",
+      "imageHash": "0x64ccc40de203f218d16bb90878ecca4338e566ab329bf7be906493ce77b1551a",
+      "mediaUrl": "https://ipfs.io/ipfs/bafybeigi3k77t5h5aefwpzvx3uiomuavdvqwn5rb5uhd7i7xcq466wvute",
+      "mediaHash": "0x64ccc40de203f218d16bb90878ecca4338e566ab329bf7be906493ce77b1551a",
+      "mediaType": "image/webp",
+      "aiMetadata": {
+        "characterFileUrl": `${process.env.NEXT_PUBLIC_ELIZA_CHARACTER_FILE}`,
+        "characterFileHash": `${process.env.NEXT_PUBLIC_ELIZA_CHARACTER_HASH}`
+      },
+      "ipType": "AI Agent", // experimental field
+      "tags": ["AI Agent", "Twitter bot", "Smart Agent"] // experimental field
+    }
+  
+    const nftMetadata = {
+      name: 'Fitness Data NFT',
+      description: 'This is an NFT representing owernship of our IP Asset.',
+      // image: 'https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg',
+      // image: 'https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      // image: 'https://images.pexels.com/photos/2827392/pexels-photo-2827392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      // image: 'https://images.pexels.com/photos/1117493/pexels-photo-1117493.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      image: 'https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    }
+  
+    const ipIpfsHash = await uploadJSONToIPFS(ipMetadata)
+    const ipHash = createHash('sha256').update(JSON.stringify(ipMetadata)).digest('hex')
+    const nftIpfsHash = await uploadJSONToIPFS(nftMetadata)
+    const nftHash = createHash('sha256').update(JSON.stringify(nftMetadata)).digest('hex')
+  
+    const response = await client.ipAsset.mintAndRegisterIpAssetWithPilTerms({
+      spgNftContract: '0xc32A8a0FF3beDDDa58393d022aF433e78739FAbc',
+      // spgNftContract: '0xfE265a91dBe911db06999019228a678b86C04959',
+      licenseTermsData: [{ terms: commercialRemixTerms, licensingConfig }], // IP already has non-commercial social remixing terms. You can add more here.
+      allowDuplicates: true,
+      ipMetadata: {
+        ipMetadataURI: `https://ipfs.io/ipfs/${ipIpfsHash}`,
+        ipMetadataHash: `0x${ipHash}`,
+        nftMetadataURI: `https://ipfs.io/ipfs/${nftIpfsHash}`,
+        nftMetadataHash: `0x${nftHash}`,
+      },
+      txOptions: { waitForTransaction: true },
+    })
+    let newMsg1 = `Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}, Token ID: ${response.tokenId}, License Terms ID: ${response.licenseTermsIds}`
+    let newMsg2 = `View on the explorer: https://aeneid.explorer.story.foundation/ipa/${response.ipId}`
+    setTxlink(`https://aeneid.explorer.story.foundation/ipa/${response.ipId}`)
+    setMsg1(newMsg1)
+    setMsg2(newMsg2)
+    
+    console.log(newMsg1)
+    console.log(newMsg2)
+  }
   }
   
   if (loading) {
@@ -171,6 +277,13 @@ export default function DashboardPage() {
               <p className="text-xl font-inter">{dashboardData?.sleep?.score}</p>
             </div>
           </div>
+        </div>
+
+        <div className="flex justify-between items-center mb-8">
+          <button
+            onClick={mintIp}
+            className="bg-blue-500 text-white px-4 py-2 mt-4"
+          >Sell fitbit data</button>
         </div>
       </div>
     </div>
